2781e95b5210cba376f60271d7a20f5115f9a441
Sung Kim <hunkim@gmail.com>
2015-07-20 20:31:39 +0800

    removed espresso for now.

--- app/src/androidTest/java/hk/ust/cse/hunkim/questionroom/JoinActivityUITest.java
+++ app/src/androidTest/java/hk/ust/cse/hunkim/questionroom/JoinActivityUITest.java
@@ -16,34 +16,14 @@ package hk.ust.cse.hunkim.questionroom;
  */
 
 
-import android.app.Activity;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
-
 /**
  * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
  * {@link ViewActions#click} and {@link ViewActions#typeText}.
  * <p/>
  * Note that there is no need to tell Espresso that a view is in a different {@link Activity}.
  */
+
+/*
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class JoinActivityUITest extends ActivityInstrumentationTestCase2 <JoinActivity>{
@@ -54,17 +34,7 @@ public class JoinActivityUITest extends ActivityInstrumentationTestCase2 <JoinAc
 
     public static final String STRING_TO_BE_TYPED = "all";
 
-    /**
-     * A JUnit {@link Rule @Rule} to launch your activity under test. This is a replacement
-     * for {@link ActivityInstrumentationTestCase2}.
-     * <p/>
-     * Rules are interceptors which are executed for each test method and will run before
-     * any of your setup code in the {@link Before @Before} method.
-     * <p/>
-     * {@link ActivityTestRule} will create and launch of the activity for you and also expose
-     * the activity under test. To get a reference to the activity you can use
-     * the {@link ActivityTestRule#getActivity()} method.
-     */
+
     @Rule
     public ActivityTestRule<JoinActivity> mActivityRule = new ActivityTestRule<>(
             JoinActivity.class);
@@ -95,3 +65,4 @@ public class JoinActivityUITest extends ActivityInstrumentationTestCase2 <JoinAc
         onView(withId(R.id.messageInput)).check(matches(withText(STRING_TO_BE_TYPED)));
     }
 }
+*/
