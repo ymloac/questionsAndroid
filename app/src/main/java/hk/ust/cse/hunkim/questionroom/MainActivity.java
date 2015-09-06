7a34ba5dc702c5775d0987ea85c7f8276e127a66
Peter Chung <hkpeterpeter@gmail.com>
2015-09-06 23:06:52 +0800

    Testing Commit in Git

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/MainActivity.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/MainActivity.java
@@ -24,6 +24,8 @@ import hk.ust.cse.hunkim.questionroom.question.Question;
 
 public class MainActivity extends ListActivity {
 
+    // Add a comment line...
+
     // TODO: change this to your own Firebase URL
     private static final String FIREBASE_URL = "https://classquestion.firebaseio.com/";
 
